a0b1272fe96b6e2051892454b9af6fc052c157ae
Samuel <samuel_monroy1@baylor.edu>
2024-03-26 16:53:51 -0500

    Revised format + no input handling

--- registration_form.py
+++ registration_form.py
@@ -22,9 +22,9 @@ class GUI:
         #---EMAIL TEXT BOX---
         self.email_frame = tkinter.Frame(self.main_window)
         self.email_label = tkinter.Label(self.email_frame, text = "Email")        
-        self.email_entry = tkinter.Entry(self.email_frame, width = 30)
+        self.email_entry = tkinter.Entry(self.email_frame, width = 25)
 
-        self.email_label.pack(side = "left")
+        self.email_label.pack(side = "left", padx = (42, 55))
         self.email_entry.pack(side = "left")
 
         self.email_frame.pack()
@@ -32,9 +32,9 @@ class GUI:
         #---PASSWORD TEXT BOX---
         self.pass_frame = tkinter.Frame(self.main_window)
         self.pass_label = tkinter.Label(self.pass_frame, text = "Password")        
-        self.pass_entry = tkinter.Entry(self.pass_frame, width = 27, show = "*")
+        self.pass_entry = tkinter.Entry(self.pass_frame, width = 25, show = "*")
 
-        self.pass_label.pack(side = "left")
+        self.pass_label.pack(side = "left", padx = (30, 40))
         self.pass_entry.pack(side = "left")
 
         self.pass_frame.pack()
@@ -42,9 +42,9 @@ class GUI:
         #---CONFIRM PASSWORD TEXT BOX---
         self.confm_pass_frame = tkinter.Frame(self.main_window)
         self.confm_pass_label = tkinter.Label(self.confm_pass_frame, text = "Confirm Password")        
-        self.confm_pass_entry = tkinter.Entry(self.confm_pass_frame, width = 21, show = "*")
+        self.confm_pass_entry = tkinter.Entry(self.confm_pass_frame, width = 25, show = "*")
 
-        self.confm_pass_label.pack(side = "left")
+        self.confm_pass_label.pack(side = "left", padx = (0, 19))
         self.confm_pass_entry.pack(side = "left")
 
         self.confm_pass_frame.pack()
@@ -97,14 +97,15 @@ class GUI:
         self.terms.pack()
         self.subs.pack()
 
-        self.term_frame.pack()
-        self.sub_frame.pack()
+        self.term_frame.pack(padx = (0, 22))
+        self.sub_frame.pack(padx = (0, 60))
 
 
         #---REGISTER BUTTON---
         self.button_frame = tkinter.Frame(self.main_window)
 
-        self.register_button = tkinter.Button(self.button_frame, text = "Register", command = self.register, font = register_font)
+        self.register_button = tkinter.Button(self.button_frame, text = "Register", command = self.register,
+                                               font = register_font, background = "blue", fg = "black", width = 30)
 
         self.register_button.pack()
         self.button_frame.pack()
@@ -114,13 +115,14 @@ class GUI:
 
     def register(self):
         regg = True
-        message1 = "Please select a gender"
-        message2 = f"Please agree with terms and" f"\n Subscribe to the newsletter"
-        message3 = "Passwords do not match"
-        message4 = "First name cannnot be blank"
-        message5 = "Last name cannot be blank"
-        message6 = "Email cannot be blank"
-        message7 = f"You have been successfully" f"\n registered"
+        message1 = "Please select a gender\n"
+        message2 = f"Please agree with terms and" f"\n Subscribe to the newsletter\n"
+        message3 = "Passwords do not match\n"
+        message4 = "First name cannnot be blank\n"
+        message5 = "Last name cannot be blank\n"
+        message6 = "Email cannot be blank\n"
+        message7 = f"You have been successfully" f"\n registered\n"
+        final_message = ""
         email =  self.email_entry.get()
         pass1 = self.pass_entry.get()
         pass2 = self.confm_pass_entry.get()
@@ -128,28 +130,29 @@ class GUI:
         last_name = self.ln_entry.get()
 
         if self.radio_var.get() == 0:
-            tkinter.messagebox.showinfo("Selection", message1)
+            final_message += message1
             regg = False
-        
+
         if self.term_var.get() == 0 or self.sub_var.get() == 0:
-            tkinter.messagebox.showinfo("Selection", message2)
+            final_message += message2
             regg = False
 
         if pass1 != pass2:
-            tkinter.messagebox.showinfo("Selection", message3)
+            final_message += message3
             regg = False
 
         if first_name == '':
-            tkinter.messagebox.showinfo("Selection", message4)
+            final_message += message4
             regg = False
 
         if last_name == '':
-            tkinter.messagebox.showinfo("Selection", message5)
+            final_message += message5
             regg = False
 
         if email == '':
-            tkinter.messagebox.showinfo("Selection", message6)
+            final_message += message6
             regg = False
+
 
         if regg:
             self.email_entry.delete(0, tkinter.END)
@@ -161,5 +164,7 @@ class GUI:
             self.term_var.set(0)
             self.sub_var.set(0)
             tkinter.messagebox.showinfo("Selection", message7)
+        else:
+            tkinter.messagebox.showinfo("Selection", final_message)
 
 regform = GUI()
